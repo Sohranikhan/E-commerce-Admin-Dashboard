9c1fcb8d3c4ca18f353bef07831cf7dd186fa1dc
Amir Sohail <sohailppp1121@gmail.com>
2025-05-18 09:49:29 +0500

    Update page.js

--- src/app/(dashboard)/[storeid]/(routes)/orders/[orderid]/page.js
+++ src/app/(dashboard)/[storeid]/(routes)/orders/[orderid]/page.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import Button from "@/components/Button/Button";
 import getSession from "@/utils/getSession";
 
 const OrderById = async () => {
