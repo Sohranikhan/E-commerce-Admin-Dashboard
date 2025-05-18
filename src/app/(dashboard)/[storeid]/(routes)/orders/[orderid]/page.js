c07706eea61dfba7e3f080f281b7437194fc5ae7
Amir Sohail <sohailppp1121@gmail.com>
2025-05-18 09:30:17 +0500

    Update page.js

--- src/app/(dashboard)/[storeid]/(routes)/orders/[orderid]/page.js
+++ src/app/(dashboard)/[storeid]/(routes)/orders/[orderid]/page.js
@@ -1,17 +1,21 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import getSession from "@/utils/getSession";
 
-const OrderById = async() => {
+const OrderById = async () => {
   const session = await getSession();
+
   if (!session) {
     return (
       <div className="flex items-center justify-center w-full h-full min-h-[36rem] mx-auto">
-        <Button className={'btn btn-primary'} text={<Link href={`/login`}>Login First</Link>} />
+        <Button asChild>
+          <Link href="/login">Login First</Link>
+        </Button>
       </div>
-    )
+    );
   }
-  return (
-    <div>OrderById</div>
-  )
-}
 
-export default OrderById
+  return <div>OrderById</div>;
+};
+
+export default OrderById;
