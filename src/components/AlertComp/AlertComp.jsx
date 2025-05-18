27469f92a9cee62dbbc65672292dac4bd78e94da
Amir Sohail <sohailppp1121@gmail.com>
2025-05-18 09:33:04 +0500

    Update AlertComp.jsx

--- src/components/AlertComp/AlertComp.jsx
+++ src/components/AlertComp/AlertComp.jsx
@@ -1,4 +1,7 @@
 "use client"
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
 const AlertComp = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [perm, setPerm] = useState({perm: 'cancel'})
